7ed2c6f1ba36e90f58000dac6e1192f7f77f301f
onerayhan <105438463+onerayhan@users.noreply.github.com>
2024-09-24 13:59:06 +0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "project_id" {
+  description = "The GCP project ID"
+}
+
+variable "region" {
+  description = "The GCP region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The GCP zone"
+  default     = "us-central1-a"
+}
